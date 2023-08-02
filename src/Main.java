03fd07ace6e6df347e215065da42ecdeca31b651
TakaC141 <takachi495@gmail.com>
2023-08-02 13:39:00 +1000

    Created Window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("~~~~~\r\n" + "Red vs. Blue\r\n" + "~~~~~");
+
+        JFrame grid = new JFrame("Grid");
+        grid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        grid.setSize(720, 720);
+
+        
+
+        grid.setVisible(true);
+
     }
+
+    
+        
+    
 }
